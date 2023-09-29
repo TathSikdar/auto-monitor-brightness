f73afba90ac43425ab4755645fec821478ee5165
prime3 <76012519+prime3333@users.noreply.github.com>
2023-09-29 19:02:13 -0400

    Update AutoBrightness.py

--- AutoBrightness.py
+++ AutoBrightness.py
@@ -2,13 +2,16 @@ import serial
 import screen_brightness_control as sbc
 import time
 
-port = 'COM7'
 
-with serial.Serial(port, baudrate = 9600, timeout = 1) as ser:
-
-    while 1:
-        time.sleep(7)
-        brightnessPercentage = ser.readline()
-        if len(brightnessPercentage)>0:
-            print(int(brightnessPercentage))
-            sbc.fade_brightness(int(brightnessPercentage))
+for portNumber in range(50):
+    port = "COM"+str(portNumber)
+    try:
+        with serial.Serial(port, baudrate = 9600, timeout = 1) as ser:
+            while 1:
+                time.sleep(7)
+                brightnessPercentage = ser.readline()
+                if len(brightnessPercentage)>0:
+                    print(int(brightnessPercentage.strip()))
+                    sbc.set_brightness(int(brightnessPercentage.strip()))
+    except Exception as e:
+        print(e)
